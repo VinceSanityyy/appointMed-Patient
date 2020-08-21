4877891870fe5faa57585c1220bae970c41ec22c
vincesanityyy <vbustillo97@gmail.com>
2020-08-21 22:18:57 +0800

    fuck shit updates

--- src/pages/AddAppointment.tsx
+++ src/pages/AddAppointment.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
-import { IonPage, IonContent, IonCard, IonCardHeader, IonCardContent, IonLabel, IonItem, IonInput, IonRow, IonCol, IonButton, IonHeader, IonToolbar, IonButtons, IonBackButton, IonTitle, useIonViewWillEnter } from '@ionic/react';
+import { IonPage, IonContent, IonCard, IonCardHeader, IonCardContent, IonLabel, IonItem, IonInput, IonRow, IonCol, IonButton, IonHeader, IonToolbar, IonButtons, IonBackButton, IonTitle, useIonViewWillEnter, IonCardSubtitle, IonCardTitle } from '@ionic/react';
 export const AddAppointment:React.FC = () =>{
+
+    const [detail, setDetail] = React.useState([])
 
     useIonViewWillEnter(()=>{
         console.log(JSON.parse(localStorage.getItem("details")))
+        setDetail(JSON.parse(localStorage.getItem("details")))
     })
+    
+    function Try(){
+        console.log(detail)
+    }
    
     return(
         <IonPage>
@@ -13,12 +20,24 @@ export const AddAppointment:React.FC = () =>{
                     <IonButtons  slot="start">
                         <IonBackButton defaultHref="/tab1" />
                     </IonButtons>
-                    <IonTitle>Add Appointment</IonTitle>
+                    <IonTitle>Add</IonTitle>
                 </IonToolbar>
             </IonHeader>
             <IonContent>
+            <IonCard>
+          <IonCardHeader>
+            <IonCardTitle>{detail['name']}</IonCardTitle>
+            <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+            
+          </IonCardHeader>
 
+          <IonCardContent>
+                asdasdasdadasdasda
+      </IonCardContent>
+        </IonCard>
+           
             </IonContent>
+            <IonButton expand="block" color="primary" onClick={Try}>Add</IonButton>
         </IonPage>
     )
 }
