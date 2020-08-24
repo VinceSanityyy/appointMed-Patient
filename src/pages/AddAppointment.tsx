3d4006386fc96b17a442874314c68bce3e035f9f
vincesanityyy <vbustillo97@gmail.com>
2020-08-24 23:56:19 +0800

    fking updates

--- src/pages/AddAppointment.tsx
+++ src/pages/AddAppointment.tsx
@@ -1,45 +1,69 @@
-import React from 'react'
-import { IonPage, IonContent, IonCard, IonCardHeader, IonCardContent, IonLabel, IonItem, IonInput, IonRow, IonCol, IonButton, IonHeader, IonToolbar, IonButtons, IonBackButton, IonTitle, useIonViewWillEnter, IonCardSubtitle, IonCardTitle } from '@ionic/react';
-export const AddAppointment:React.FC = () =>{
+import React from "react";
+import {
+  IonPage,
+  IonContent,
+  IonCard,
+  IonCardHeader,
+  IonCardContent,
+  IonLabel,
+  IonItem,
+  IonInput,
+  IonRow,
+  IonCol,
+  IonButton,
+  IonHeader,
+  IonToolbar,
+  IonButtons,
+  IonBackButton,
+  IonTitle,
+  useIonViewWillEnter,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonAvatar,
+  IonDatetime,
+} from "@ionic/react";
+export const AddAppointment: React.FC = () => {
+  const [detail, setDetail] = React.useState([]);
+  const [selectedDate, setSelectedDate] = React.useState('')
 
-    const [detail, setDetail] = React.useState([])
+  useIonViewWillEnter(() => {
+    console.log(JSON.parse(localStorage.getItem("details")));
+    setDetail(JSON.parse(localStorage.getItem("details")));
+  });
 
-    useIonViewWillEnter(()=>{
-        console.log(JSON.parse(localStorage.getItem("details")))
-        setDetail(JSON.parse(localStorage.getItem("details")))
-    })
-    
-    function Try(){
-        console.log(detail)
-    }
-   
-    return(
-        <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    <IonButtons  slot="start">
-                        <IonBackButton defaultHref="/tab1" />
-                    </IonButtons>
-                    <IonTitle>Add</IonTitle>
-                </IonToolbar>
-            </IonHeader>
-            <IonContent>
-            <IonCard>
+  function Try() {
+    console.log(selectedDate);
+  }
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonBackButton defaultHref="/tab1" />
+          </IonButtons>
+          <IonTitle>Add</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonCard>
           <IonCardHeader>
-            <IonCardTitle>{detail['name']}</IonCardTitle>
-            <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
-            
+            <IonCardTitle>Dr. {detail["name"]}</IonCardTitle>
+            <IonCardSubtitle>
+              Speciality: {detail["speciality"]}
+            </IonCardSubtitle>
           </IonCardHeader>
-
-          <IonCardContent>
-                BULLSHIT
-      </IonCardContent>
+          <IonCardContent>Test
+          <IonLabel>MM DD YY</IonLabel>
+          <IonDatetime displayFormat="MM DD YY" placeholder="Select Date" value={selectedDate} onIonChange={e => setSelectedDate(e.detail.value!)}></IonDatetime>
+          </IonCardContent>
         </IonCard>
-           
-            </IonContent>
-            <IonButton expand="block" color="primary" onClick={Try}>Add</IonButton>
-        </IonPage>
-    )
-}
+        <IonButton expand="block" color="primary" onClick={Try}>
+          Add
+        </IonButton>
+      </IonContent>
+    </IonPage>
+  );
+};
 
 export default AddAppointment;
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonSearchbar, IonButton, useIonViewDidEnter, useIonViewWillEnter, IonList, IonItem, IonLabel, IonRefresher, IonRefresherContent, IonText, useIonViewDidLeave, IonAvatar } from '@ionic/react';
-// import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
 import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps' 
 import * as firebase from 'firebase/app'
